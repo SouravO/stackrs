fe4fe542e2d780ac80b05ced4e66070edb62ff60
abdulamal15 <amal.abdulla15@gmail.com>
2026-06-06 02:11:15 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -257,7 +257,7 @@ export default function EnhancedHero({ onJoinClick, onWatchDrawClick }) {
   <img
     src="/stackr-logo.png.png"
     alt="Stackr"
-    className="h-14 w-auto"
+    className="h-20 w-auto"
   />
 </motion.div>
 
